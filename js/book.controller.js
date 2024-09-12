a090a97cce62294f4d499796b30b9b2995d56153
LeonPr <leon@masig.co.il>
2024-09-12 16:13:29 +0300

    Add render action

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,10 +1,23 @@
 
 
 
-function onInit(){
+function onInit() {
     render()
 }
 
-function render(){
+function render() {
+
+    const booksData = getBooks()
+    const elBook = document.querySelector('.book-data')
+
+
+    const strHTML = booksData.map(book => `<tr><td>${book.title}</td>
+                <td>${book.price}</td>
+                <td>
+                    <button class="read" onclick="onReadClick( this,${book.id})">Read</button>
+                    <button class="update" onclick="onReadClick( this,${book.id})">Update</button>
+                    <button class="delete" onclick="onReadClick( this,${book.id})">Delete</button>
+                </td></tr>`)
+    elBook.innerHTML = strHTML.join('')
 
 }
