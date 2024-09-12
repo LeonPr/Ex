3ed824265c29c89e4eb49fc4a181dc0570299be5
LeonPr <leon@masig.co.il>
2024-09-12 17:31:12 +0300

    Add update functions

--- js/book.controller.js
+++ js/book.controller.js
@@ -11,17 +11,39 @@ function render() {
     const strHTML = booksData.map(book => `<tr><td>${book.title}</td>
                 <td>${book.price}</td>
                 <td>
-                    <button class="read" onclick="onReadClick( this,${book.id})">Read</button>
-                    <button class="update" onclick="onUpdateBook( this,${book.id})">Update</button>
+                    <button class="read" onclick="onReadClick( this,'${book.id}')">Read</button>
+                    <button class="update" onclick="onUpdateBook( this,'${book.id}')">Update</button>
                     <button class="delete" onclick="onRemoveBook( this,'${book.id}')">Delete</button>
                 </td></tr>`)
 
     elBook.innerHTML = strHTML.join('')
 }
-function onRemoveBook(elBook,bookId){
+
+function onRemoveBook(elBook, bookId) {
     RemoveBook(bookId)
     render()
 }
-function onUpdateBook(elBook,bookId){
-    
+
+function onUpdateBook(elBook, bookId) {
+    openDialog(bookId)
+}
+
+function openDialog(bookId) {
+
+    const elDialog = document.querySelector('.update-dialog')
+    const elDetails = elDialog.querySelector('pre span')
+
+    elDetails.innerText = bookId
+    elDialog.showModal()
+}
+
+function onUpdateClick(ev) {
+    ev.stopPropagation()
+    const elDialog = document.querySelector('.update-dialog')
+    const elDetails = elDialog.querySelector('pre span')
+    const elInput = document.querySelector('input')
+
+    updateBook(elInput.value,elDetails.innerText)
+    elInput.value=''
+    render()
 }
--- js/services/book.service.js
+++ js/services/book.service.js
@@ -1,16 +1,21 @@
 'use strict'
 
-var gBooks=[
-    {id:'b00',title:'The Adventures of Lori Ipsi',price:120},
-    {id:'b01',title:'Word Atlas',price:120},
-    {id:'b02',title:'Zorba The Greek',price:120},
+var gBooks = [
+    { id: 'b00', title: 'The Adventures of Lori Ipsi', price: 120 },
+    { id: 'b01', title: 'Word Atlas', price: 120 },
+    { id: 'b02', title: 'Zorba The Greek', price: 120 },
 ]
 
 
-function getBooks(){
-        return gBooks
+function getBooks() {
+    return gBooks
 }
-function RemoveBook(bookId){
-    const delIndx=gBooks.findIndex(book => gBooks.id===bookId)
-    gBooks.splice(delIndx,1)
+function removeBook(bookId) {
+    const indx = gBooks.findIndex(book => book.id === bookId)
+    gBooks.splice(indx, 1)
+}
+
+function updateBook(newPrice, bookId) {
+    const updateBook = gBooks.find(book => book.id === bookId)
+    updateBook.price = newPrice
 }
