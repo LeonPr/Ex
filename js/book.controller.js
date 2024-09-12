2056490672956e9a609047ed8a421d41651840cc
LeonPr <leon@masig.co.il>
2024-09-12 14:52:55 +0300

    Add service and controller js

---
+++ js/book.controller.js
@@ -0,0 +1,10 @@
+
+
+
+function onInit(){
+    render()
+}
+
+function render(){
+
+}
---
+++ js/services/book.service.js
@@ -0,0 +1,10 @@
+var gBooks=[
+    {id:'b00',title:'The Adventures of Lori Ipsi',price:120},
+    {id:'b01',title:'Word Atlas',price:120},
+    {id:'b02',title:'Zorba The Greek',price:120},
+]
+
+
+function getBooks(){
+        return gBooks
+}
