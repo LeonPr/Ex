db89cadeb600454a9111da324fea584e1b7c8101
LeonPr <leon@masig.co.il>
2024-09-13 13:33:53 +0300

    Add render js

--- js/render.controler.js
+++ js/render.controler.js
@@ -1,10 +1,5 @@
-'use strict'
 
-function onInit() {
-    render()
-}
-
-/*function render() {
+function render() {
     const booksData = getBooks()
     const elBook = document.querySelector('.book-data')
 
@@ -50,63 +45,4 @@ function renderDetailsDialog(bookId) {
                       <button class="new-book read" >close</button>
                   </form>`
     elDialog.innerHTML = strHTML
-}*/
-
-function onRemoveBook(elBook, bookId) {
-    removeBook(bookId)
-    render()
-}
-
-function onUpdateBook(elBook, bookId) {
-    openUpdateDialog(bookId)
-}
-
-function onAddBook(elAdd) {
-    renderAddDialog()
-    const elDialog = document.querySelector('.update-dialog')
-    elDialog.showModal()
-}
-
-function onReadClick(elBook, bookId) {
-    renderDetailsDialog(bookId)
-    const elDialog = document.querySelector('.update-dialog')
-    elDialog.showModal()
-}
-
-function onReadSubmit(ev) {
-    ev.stopPropagation()
-}
-
-function onAddSubmit(ev) {
-    ev.stopPropagation()
-
-    const inputName = document.getElementById('name')
-    const inputPrice = document.getElementById('price')
-    if (inputName.value === '' || inputPrice.value === '') {
-        alert('Both values must be entered')
-        return
-    }
-    insertBook(inputName.value, inputPrice.value)
-    render()
-}
-
-function openUpdateDialog(bookId) {
-    renderUpdateDialog()
-    const elDialog = document.querySelector('.update-dialog')
-    const elDetails = elDialog.querySelector('pre span')
-
-    elDetails.innerText = bookId
-    elDialog.showModal()
-}
-
-function onUpdateClick(ev) {
-    ev.stopPropagation()
-
-    const elDialog = document.querySelector('.update-dialog')
-    const elDetails = elDialog.querySelector('pre span')
-    const elInput = document.querySelector('input')
-
-    updateBook(elInput.value, elDetails.innerText)
-    elInput.value = ''
-    render()
 }
